853bc3e891e56cd341e758e6868618847cdb7f2f
John Anthony Rivera <askinganthony@gmail.com>
2016-02-13 17:36:19 -0800

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function addIngredient(recipe, name, amount) {
+	recipe[name]=amount;
+	return recipe;
+}
+
+function removeIngredient(recipe, key) {
+	delete recipe[key];
+	return recipe;
+}
+
+function updateIngredient(recipe, key, newValue) {
+	recipe[key]=newValue;
+	return recipe;
+}
+
+function readRecipe(recipe) {
+	var array = [];
+	for (var ing in recipe) { console.log("this recipe calls for "+recipe[ing]+" of "+ing); }
+	//debugger;
+	return array;
+}
